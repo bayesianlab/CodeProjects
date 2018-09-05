d8594dd15bd766bced3be761f579506a99005c06
Dillon <dillonflann@gmail.com>
2018-09-05 11:43:59 -0700

    a

--- R/section_summer2018_Sept3.R
+++ R/section_summer2018_Sept3.R
@@ -1,5 +1,5 @@
 library(sandwich)
-
+read.csv("https://eee.uci.edu/18z/62220/home/wagedata.csv")
 # Dummy Variable Reg
 N <- 100
 x<-1:N+rnorm(N,0,5)
@@ -12,17 +12,24 @@ r1 <- lm(y~x+z + x*z)
 summary(r1)
 
 D <- cbind(y,x,z)
+D
+
 colnames(D) <- c("Wage", "Ability", "Phd")
-write.csv2(D,file="wagedata.csv",row.names=FALSE)
+D
+
+write.csv(D,file="wagedata.csv", row.names=FALSE)
 # Heteroskedasticity 
 
 x<-1:100+rnorm(100,0,10)
 z <- c(rep(1,25), rep(0,75))
 Het <- rep(1,100)
 Het <- c(Het[1:25], Het[26:50]*rep(2,25), Het[51:100]*rep(4,50)) 
-y <- 3 + 1.2*x + 5.4*z + rnorm(100,0,sd=10)*Het
+y <- 3 + 1.2*x + rnorm(100,0,sd=10)*Het
 plot(x,y)
 r2 <- lm(y~x+z)
 vcov(r2)
 sqrt(diag(vcovHC(r2)))
 summary(r2)
+
+write.csv(cbind(y,x), file="ec122A.csv")
+
