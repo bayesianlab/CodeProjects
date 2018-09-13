e9eeedc598ff8e0a2fe09fec0f80c70dccb378f3
HPC <flannerd@uci.edu>
2018-09-12 17:51:58 -0700

    h

--- MATLAB/ML/ghkmvnrnd.m
+++ MATLAB/ML/ghkmvnrnd.m
@@ -23,6 +23,23 @@ if sum(~isfinite(z)) > 0
     L
     z
     na = 1;
+    [J,~] = size(sigma);
+L = chol(sigma, 'lower');
+offDiagonals = tril(L, -1);
+yDim = 1:J;
+eta = zeros(J,N);
+for i = 1:N
+    for j = yDim
+        update = mu(j) + (offDiagonals(j,:)*eta(:,i))
+        aj = (a(j) - update)/L(j,j)
+        bj = (b(j) - update)/L(j,j)
+        Fb = normcdf(bj,0,1)
+        Fa = normcdf(aj,0,1)
+        eta(j,i) = norminv(unifrnd(0,1,1)*(Fb-Fa) + Fa)
+    end
+end
+z = (L*eta + mu)
+save('ErrorOutput.mat')
 else
     na =0;
 end
