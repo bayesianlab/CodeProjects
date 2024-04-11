642dbf16ea84923fbdf68e7e4cf368e6025e95d8
Dillon Flannery-Valadez <dillon.flanneryvaladez@amerihome.com>
2024-04-11 15:03:11 -0700

    M

---
+++ CProjects/ann/ann.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+class Input
+{
+public:
+	template<typename T> 
+	Input(MatrixXd X, T Activation)
+	{
+		int InputRows = X.rows();
+		int InputCols = X.cols(); 
+		
+	}
+};
+
+
+int main()
+{
+	cout << "ann" << endl; 
+}
