47ecddc20128076369c68f7b9d045f4fedda9550
Dillon Valadez <dillonflann@gmail.com>
2024-04-22 07:31:10 -0700

    M

--- CProjects/ann/src/ann.cpp
+++ CProjects/ann/src/ann.cpp
@@ -19,39 +19,40 @@ double logistic_derivative(VectorXd layer_states, VectorXd weights)
 	return fx * (1 - fx);
 }
 
-class Node
-{
-public:
-	double NodeValue;
-	VectorXd weights;
-	int dimension_zm1; 
+// class Node
+// {
+// public:
+// 	double NodeValue;
+// 	VectorXd weights;
+// 	int dimension_zm1; 
+// 	function<double(VectorXd, VectorXd)> activation;
 
-	Node(int input_dim, VectorXd &z_lm1,function<double(VectorXd, VectorXd)> func)
-	{	
-		// All other layers	
-		dimension_zm1 = input_dim; 
-		initialize_weights();
-		compute_self(z_lm1, func);
-	}
+// 	Node(int input_dim, function<double(VectorXd, VectorXd)> func)
+// 	{	
+// 		// All other layers	
+// 		activation = func; 
+// 		dimension_zm1 = input_dim; 
+// 		weights = unifrnd(-1,1, dimension_zm1);
+// 	}
 
-	Node(double x)
-	{
-		// Should be z_0 layer. Initialize z_0
-		NodeValue = x;
-	}
+// 	Node()
+// 	{
+// 		// Should be z_0 layer. Initialize z_0
+// 		NodeValue = x;
+// 	}
 
-	void initialize_weights()
-	{
-		weights = unifrnd(-1,1, dimension_zm1);
-	}
+// 	void initialize_weights()
+// 	{
+		
+// 	}
 
 	
-	void compute_self(VectorXd z_lm1, function<double(VectorXd, VectorXd)> ActivationFunc)
-	{
-		NodeValue = ActivationFunc(z_lm1, weights); 
-	}
+// 	void compute_self(VectorXd z_lm1, function<double(VectorXd, VectorXd)> ActivationFunc)
+// 	{
+// 		NodeValue = ActivationFunc(z_lm1, weights); 
+// 	}
 
-};
+// };
 
 
 class Layer
@@ -61,43 +62,29 @@ public:
 
 	double threshold = 0.5;
 	VectorXd zl; 	 
-	vector<Node> neurons; 
-	int output_dim;
-	
-	void create_layer(int n_nodes, Layer &ConnectedLayer, function<double(VectorXd, VectorXd)> func)
+	function<double(VectorXd, VectorXd)> activation;
+	MatrixXd Weights; 
+
+	void create_layer(int n_nodes, const Layer &ConnectedLayer, const function<double(VectorXd, VectorXd)> &func)
 	{
-		neurons.clear(); 
-		output_dim = n_nodes; 
-		zl = VectorXd::Zero(n_nodes); 
-		for(int i = 0; i < n_nodes; ++i)
-		{
-			Node n = Node(ConnectedLayer.output_dim, ConnectedLayer.zl, func);
-			neurons.push_back(n);
-			zl(i) = n.NodeValue;
-		}
+		activation = func; 
+		Weights = unifrnd(0,1,n_nodes, ConnectedLayer.zl.size());
+		zl.resize(n_nodes);
 	}
 
-	void create_layer_0(VectorXd x)
+	void create_input_layer(int n_nodes)
 	{
-		neurons.clear();
-		output_dim = x.size();
-		zl = VectorXd::Zero(output_dim); 
-		for(int i = 0; i < x.size(); ++i)
-		{
-			zl(i) = x(i); 
-			neurons.push_back(Node(x(i)));
-		}
+		zl.resize(n_nodes); 
 	}
 
-	void compute_self(VectorXd z_lm1, function<double(VectorXd, VectorXd)> func)
+	void compute_self(Layer &ConnectedLayer)
 	{
-		
-		for (int i = 0; i < neurons.size(); ++i)
-		{
-			neurons[i].compute_self(z_lm1, func);
-			zl(i) = neurons[i].NodeValue; 
-		}
-		cout << endl; 
+		zl = Weights * ConnectedLayer.zl; 
+	}
+
+	void compute_self(VectorXd &x)
+	{
+		zl = x; 
 	}
 };
 
@@ -110,55 +97,90 @@ class Network
 		layers.push_back(L);
 	}
 
-	double sum_dcost(VectorXd &targets, double weight,
-					   function<double(VectorXd, VectorXd)> ActivationFunc,
-				   	   int layer_index, int i, int j)
+	VectorXd compute_net(const MatrixXd &X)
 	{
-		double cost = 0;
-		for(int i = 0; i < targets.size(); ++i)
+		VectorXd yhat(X.rows());
+		for (int i = 0; i < X.rows(); ++i)
 		{
-			// cost += dcost_dzl(weight, targets(i), ActivationFunc, layer_index, i ,j);
+			for (int k = 0; k < layers.size(); ++k)
+			{
+				if (k == 0)
+				{
+					VectorXd z0 = X.row(i);
+					layers[0].compute_self(z0);
+				}
+				else if (k == layers.size() - 1)
+				{
+					layers[k].compute_self(layers[k - 1]);
+					yhat(i) = layers[k].zl.value();
+				}
+				else
+				{
+					layers[k].compute_self(layers[k - 1]);
+				}
+			}
 		}
-		return cost;
+		return yhat;
 	}
 
-
-	double dcost_dzl(double weight, const VectorXd &targets,
-				   function<double(VectorXd, VectorXd)> ActivationFunc,
-				   int layer_index, int i, int j)
+	void compute_net(const MatrixXd &X, int start_layer)
 	{
-		// Compute the change in cost for a chanage in z
-		if((layer_index < 1) || (layer_index>=layers.size()))
+		VectorXd yhat(X.rows());
+		for (int i = 0; i < X.rows(); ++i)
 		{
-			throw invalid_argument("Can only use Layers 1,...,L");
+			for (int k = start_layer; k < layers.size(); ++k)
+			{
+				if (k == 0)
+				{
+					VectorXd z0 = X.row(i);
+					layers[0].compute_self(z0);
+				}
+				else if (k == layers.size() - 1)
+				{
+					layers[k].compute_self(layers[k - 1]);
+					yhat(i) = layers[k].zl.value();
+				}
+				else
+				{
+					layers[k].compute_self(layers[k - 1]);
+				}
+			}
 		}
-		layers[layer_index].neurons[i].weights(j) = weight;
+	}
+
+	void compute_error_grad(const MatrixXd &X, const VectorXd &targets)
+	{
+		VectorXd yhat = compute_net(X); 
 		
-		layers[layer_index].neurons[i].compute_self(layers[layer_index - 1].zl, ActivationFunc);
-		int li = layer_index; 
-		while(li < layers.size()-1)
+		for(int i = layers.size()-1; i >=0; --i)
 		{
-			layers[li+1].compute_self(layers[li].zl, ActivationFunc);
-			++li; 
-		}
-	}
-
-	void backpropagation(MatrixXd &Samples, VectorXd &target)
-	{
-		
-		int endex = layers.size()-1;
-		double delta_l = 0;
-		for(int i = 0; i < Samples.rows(); ++i)
-		{
-			delta_l += layers[endex].zl(0) - target(i);
+			if(i==layers.size()-1)
+			{
+				VectorXd error = yhat - targets; 
+			}
+			else
+			{
+				
+			}
 		}
 
-		// for(auto it = layers.size()-1; it>-1; it--)
-		// {
-
-		// 	break; 
-		// }
 	}
+
+	// double compute_dcost(VectorXd &targets, MatrixXd &X, 
+	// 				   function<double(VectorXd, VectorXd)> ActivationFunc,
+	// 			   	   int layer_index, int i, int j)
+	// {
+	// 	double cost = 0;
+	// 	for(int i = 0; i < targets.size(); ++i)
+	// 	{
+			
+	// 	}
+	// 	return cost;
+	// }
+
+	// void backpropagation(MatrixXd &Samples, VectorXd &target)
+	// {
+	// }
 };
 
 
@@ -166,8 +188,9 @@ int main()
 {
 
 	cout << "ann" << endl; 
-	int N = 100;
+	int N = 2;
 	MatrixXd X = normrnd(0,1,N,10);
+	cout << X << endl; 
 	VectorXd w = unifrnd(-1,1, 10);
 	VectorXd y = X*w + normrnd(0,1,N,1);
 	
@@ -177,7 +200,7 @@ int main()
 	Layer L2; 
 	Layer LL; 
 	Network Net; 
-	L0.create_layer_0(X.row(0));
+	L0.create_input_layer(X.cols());
 	Net.add_layer(L0);
 	L1.create_layer(10, L0, logistic_activation);
 	Net.add_layer(L1);
@@ -186,7 +209,8 @@ int main()
 	LL.create_layer(1, L2, logistic_activation);
 	Net.add_layer(LL);
 
-	cout << Net.sum_dcost(y, 5, logistic_activation, 2, 0, 3) << endl;
+	cout << Net.compute_net(X) << endl;
+	Net.compute_error_grad(X, y) ; 
 
 
 }
